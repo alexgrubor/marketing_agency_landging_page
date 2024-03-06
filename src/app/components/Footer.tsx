c23babce24fc2613eafc995b78f4d5d1f230cb78
Alex <alexgrubor@gmail.com>
2024-03-06 13:35:28 +0100

    footer

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link"
+const Footer = () => {
+return (
+<footer className="bg-bg text-secondBg p-8">
+        
+        <div className="container mx-auto flex flex-col md:flex-row justify-between">
+            <div className="mb-4 md:mb-0">
+                <h2 className="text-lg font-bold mb-2">Agency</h2>
+                <ul>
+                    <li><Link href="#">Our Services</Link></li>
+                    <li><Link href="#">Contact Us</Link></li>
+                    <li>
+                        <Link href="#">Our Privacy Policy</Link>
+                    </li>
+
+                </ul>
+            </div>
+            <div className="mb-4 md:mb-0">
+                <h2 className="text-lg font-bold mb-2">Company</h2>
+                <ul>
+                    <li><Link href="#">About Us</Link></li>
+                    <li><Link href="#">Careers</Link></li>
+                </ul>
+            </div>
+            <div>
+                <h2 className="text-lg font-bold mb-2">Resources</h2>
+                <ul>
+                    <li><Link href="#">Blog</Link></li>
+                    <li><Link href="#">Newsletter</Link></li>
+                </ul>
+            </div>
+        </div>
+        <p className="text-center my-3">
+            &copy; 2024 Agency. All rights reserved.
+        </p>
+    </footer>
+)
+}
+export default Footer
--- src/app/components/ui/BrandsInfiniteScroll.tsx
+++ src/app/components/ui/BrandsInfiniteScroll.tsx
@@ -1,18 +1,17 @@
 import brands from "../../../../public/brands";
 import Image from "next/image";
-
 import Marquee from "react-fast-marquee";
 
 const BrandsInfiniteScroll = () => {
   return (
-    <div className="bg-secondBg ">
+    <div className="bg-secondBg z-[3] ">
       {
         <Marquee gradient={false} speed={40}>
           {brands.map((brand, index) => (
             <div key={index} className="mx-4">
               <Image
                 src={brand.image}
-                alt={brand.id + ""}
+                alt={brand.id + "brand"}
                 width={300}
                 height={300}
               />
