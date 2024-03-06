85fd2db7d7d733c7b05ff6676ff4c97ed476765d
Alex <alexgrubor@gmail.com>
2024-03-06 12:27:49 +0100

    delete of unused divs

--- src/app/components/ui/BrandsInfiniteScroll.tsx
+++ src/app/components/ui/BrandsInfiniteScroll.tsx
@@ -5,24 +5,21 @@ import Marquee from "react-fast-marquee";
 
 const BrandsInfiniteScroll = () => {
   return (
-    <div className="bg-secondBg p-4 flex flex-col justify-center items-center gap-4">
-    
-      <div>
-        {
-          <Marquee gradient={false} speed={40}>
-            {brands.map((brand, index) => (
-              <div key={index} className="mx-4">
-                <Image
-                  src={brand.image}
-                  alt={brand.id + ''}
-                  width={300}
-                  height={300}
-                />
-              </div>
-            ))}
-          </Marquee>
-        }
-      </div>
+    <div className="bg-secondBg ">
+      {
+        <Marquee gradient={false} speed={40}>
+          {brands.map((brand, index) => (
+            <div key={index} className="mx-4">
+              <Image
+                src={brand.image}
+                alt={brand.id + ""}
+                width={300}
+                height={300}
+              />
+            </div>
+          ))}
+        </Marquee>
+      }
     </div>
   );
 };
