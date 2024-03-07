652fb47c8d7cfa9934b7cd2248721da19d101581
Alex <alexgrubor@gmail.com>
2024-03-07 15:13:05 +0100

    nuber animation fix

--- src/app/components/Numbers.tsx
+++ src/app/components/Numbers.tsx
@@ -1,24 +1,53 @@
 'use client'
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 interface NumbersProps {
-  targetNumber: number; 
+  targetNumber: number;
 }
 
-const Numbers = ({ targetNumber } : NumbersProps) => {
-    const [currentNumber, setCurrentNumber] = useState(0);
+const Numbers = ({ targetNumber }: NumbersProps) => {
+  const [currentNumber, setCurrentNumber] = useState(0);
+  const numbersRef = useRef<HTMLDivElement>(null);
 
-    useEffect(() => {
-      const intervalId = setInterval(() => {
-        setCurrentNumber((prev) => Math.min(prev + 1, targetNumber)); 
-      }, 30); 
-  
-      return () => clearInterval(intervalId); 
-    }, [currentNumber, targetNumber]);
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            console.log('intersecting');
+
+            const intervalId = setInterval(() => {
+              setCurrentNumber((prev) => {
+                const nextNumber = Math.min(prev + 1, targetNumber);
+                if (nextNumber === targetNumber) {
+                  clearInterval(intervalId); // Clear the interval when target number is reached
+                }
+                return nextNumber;
+              });
+            }, 30);
+          } else {
+            setCurrentNumber(0);
+            console.log('not intersecting');
+          }
+        });
+      },
+      { rootMargin: '100px' }
+    );
+
+    if (numbersRef.current) {
+      observer.observe(numbersRef.current);
+    }
+
+    return () => {
+      if (numbersRef.current) {
+        observer.unobserve(numbersRef.current);
+      }
+    };
+  }, [targetNumber]);
 
   return (
-    <div className="text-9xl text-myPurple-700 font-bold ml-9 inline">
-      {currentNumber}% 
+    <div ref={numbersRef} className="text-9xl text-myPurple-700 font-bold ml-9 inline">
+      {currentNumber}%
     </div>
   );
 };
