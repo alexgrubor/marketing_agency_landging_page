5823e5968e73bb11ecf621fa9d636168fdd7e0a6
Alex <alexgrubor@gmail.com>
2024-03-11 14:31:29 +0100

    testemonial responsivity fix

--- src/app/about-us/components/Testemonials.tsx
+++ src/app/about-us/components/Testemonials.tsx
@@ -1,5 +1,7 @@
+'use client'
 import Marquee from "react-fast-marquee";
 import Image from "next/image";
+import useResponsive from "@/hooks/useResponsive";
 const clients = [
   {
     name: 'John Doe',
@@ -39,12 +41,13 @@ const clients = [
   },
 ];
 const Testemonials = () => {
+  const deviceType = useResponsive();
   return (
     <div className="bg-secondBg z-[3] p-4">
     <h2 className="text-3xl text-center text-myPurple-600 font-bold my-8">
       Testimonials
     </h2>
-    <Marquee gradient={false} speed={40}>
+    {deviceType !== 'mobile' && <Marquee gradient={false} speed={40}>
       {clients.map((client, index) => (
         <div key={index} className="flex flex-col sm:flex-row border-2 shadow-sm shadow-myPurple-900 border-myPurple-600 m-3 items-center gap-4 p-3">
           <div className="flex-shrink-0">
@@ -57,7 +60,21 @@ const Testemonials = () => {
           </div>
         </div>
       ))}
-    </Marquee>
+    </Marquee>}
+    {
+      deviceType === 'mobile' && clients.map((client, index) => (
+        <div key={index} className="flex flex-col sm:flex-row border-2 shadow-sm shadow-myPurple-900 border-myPurple-600 m-3 items-center gap-4 p-3">
+          <div className="flex-shrink-0">
+            <Image src={client.avatarPath} alt={`${client.name}'s Avatar`} width={100} height={100} className="rounded-full" />
+          </div>
+          <div className="flex flex-col justify-center items-center sm:items-start sm:pl-4">
+            <h3 className="text-myPurple-900 text-lg font-semibold">{client.name}</h3>
+            <p className="text-black font-bold text-sm">{client.company}</p>
+            <p className="text-black text-sm p-5">"{client.review}"</p>
+          </div>
+        </div>
+      ))
+    }
   </div>
   )
 }
