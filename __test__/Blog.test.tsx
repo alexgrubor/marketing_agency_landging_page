71d3170f1492345beaef50fcfc3f08465dd6522a
Alex <alexgrubor@gmail.com>
2024-03-13 11:58:11 +0100

    Changed the component being tested from `Blogs` to `Blog`. Added a snapshot test for the `BlogCard` component.

--- __test__/Blog.test.tsx
+++ __test__/Blog.test.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
-import Blogs from '../src/app/blogs/page'
+import Blog from '../src/app/blogs/page'
+import Blogs from '@/app/blogs/components/Blogs';
+import BlogCard from '@/app/blogs/components/BlogCard';
+
 import '@testing-library/jest-dom'; 
 test('renders the title', () => {
-  render(<Blogs />)
+  render(<Blog />)
     const title = screen.getByText(/Blogs/i)
     expect(title).toBeInTheDocument();
 })
+
+//snapshot 
+test('expect to render BlogCard', () => {
+    const blog = {
+        id: 1,
+        title: 'title',
+        body: 'body'
+        
+    }
+    const { container } = render(<BlogCard blog={blog} />)
+    expect(container).toMatchSnapshot()
+
+}) 
 
 
