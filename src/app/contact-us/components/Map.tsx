887a7ccdf1d1a108afbb8a52fc05be7359839b9f
Alex <alexgrubor@gmail.com>
2024-03-14 12:29:29 +0100

    fix for map

--- src/app/contact-us/components/Map.tsx
+++ src/app/contact-us/components/Map.tsx
@@ -1,58 +1,24 @@
 "use client";
-import { useState, useCallback } from "react";
-import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
 
-const containerStyle = {
-  width: "300px",
-  height: "300px",
-  borderRadius: '4px'
-};
+import { GoogleMapsEmbed } from "@next/third-parties/google";
 
-const center = {
-  lat: 52.52437,
-  lng: 13.41053,
-};
 const Map = () => {
-  const { isLoaded } = useJsApiLoader({
-    id: "google-map-script",
-    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY ?? "",
-  });
- 
-  const [map, setMap] = useState<google.maps.Map | null>(null); 
-
-
-  const onLoad = useCallback(function callback(map: google.maps.Map) {
-    const bounds = new window.google.maps.LatLngBounds();
-    map.fitBounds(bounds);
-    setMap(map);
-  }, []);
-  const onUnmount = useCallback(function callback(map: google.maps.Map) {
-    setMap(null);
-  }, []);
-  return isLoaded ? (
+  return (
     <div className="flex flex-col md:flex-row custom-bg text-secondBg  w-[100vw] justify-around p-4 items-center gap-8">
-        <div className="text-2xl md:pl-[6rem]">
-            <h2 className="text-myPurple-600 my-2">Our Address</h2>
-            <p>1234, Street Name</p>
-            <p>Berlin</p>
-            <p>Berlin, Germany</p>
-
-        </div>
-<GoogleMap
-      mapContainerStyle={containerStyle}
-      center={center}
-      zoom={10}
-      onLoad={onLoad}
-      onUnmount={onUnmount}
-    >
-        <Marker position={center} />
-    </GoogleMap>
+      <div className="text-2xl md:pl-[6rem]">
+        <h2 className="text-myPurple-600 my-2">Our Address</h2>
+        <p>1234, Street Name</p>
+        <p>Berlin</p>
+        <p>Berlin, Germany</p>
+      </div>
+      <GoogleMapsEmbed
+      apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY ?? ""}
+      height={200}
+      width="100%"
+      mode="place"
+      q="Berlin"
+    />
     </div>
-    
-  ) : (
-    <>
-      <h2>Loading....</h2>
-    </>
   );
 };
 export default Map;
