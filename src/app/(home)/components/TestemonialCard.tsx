561f95d77da7f97170535d9d418a1cdf9a430cb9
Alex <alexgrubor@gmail.com>
2024-03-18 11:20:51 +0100

    Add testimonials data and TestimonialCard component, refactor Testimonial

---
+++ src/app/(home)/components/TestemonialCard.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+
+interface TestimonialCardProps {
+    quote: string;
+    name: string;
+    title: string;
+    company: string;
+    image: string;
+    
+}
+
+const TestimonialCard = ({ quote, name, title, company, image } :TestimonialCardProps) => {
+  return (
+    <div className="p-6 rounded shadow-md shadow-myPurple-600 dark:bg-gray-900">
+      <p>{quote}</p>
+      <div className="flex items-center mt-4 space-x-4">
+        <Image
+          src={image}
+          alt={name}
+          width={50}
+          height={50}
+          layout="fit"
+          className="w-12 h-12 rounded-full"
+        />
+        <div>
+          <p className="text-lg font-semibold">{name}</p>
+          <p className="text-sm dark:text-gray-400">{title}, {company}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TestimonialCard;
--- src/app/(home)/components/Testimonials.tsx
+++ src/app/(home)/components/Testimonials.tsx
@@ -1,4 +1,66 @@
 import Image from "next/image";
+import TestimonialCard from "./TestemonialCard";
+
+const testimonialsData = [
+  {
+    quote: `Working with Our Marketing Agency has been a game-changer
+    for our company. Their innovative strategies and attention
+    to detail have significantly boosted our online presence and
+    customer engagement. The team at Our Marketing is not only
+    highly skilled but also incredibly responsive and dedicated.
+    I highly recommend their services to any business looking to
+    take their marketing efforts to the next level`,
+    name: "Sarah Johnson",
+    title: "CEO", 
+    company: "Techify Inc.",
+    image: "/team_icons/team1.svg"
+  },
+  {
+    quote: `Our Marketing Agency exceeded our expectations in every
+    aspect. From the initial consultation to the execution of
+    our marketing campaigns, they demonstrated professionalism
+    and expertise. Their ability to understand our brand and
+    target audience resulted in tangible results, including
+    increased website traffic and higher conversion rates. I'm
+    extremely satisfied with the results and look forward to
+    continuing our partnership`,
+    name: "Michael Smith",
+    title: "Marketing Manager",
+    company: "FreshBites",
+    image: "/team_icons/team4.svg"
+  },
+  {
+    quote:`  As a small business owner, finding the right marketing
+    partner was crucial for our growth. Our Marketing Agency
+    came highly recommended, and they did not disappoint. Their
+    team took the time to understand our unique needs and
+    crafted customized strategies that aligned perfectly with
+    our goals. Their creativity and attention to detail set them
+    apart, and we've seen a significant improvement in brand
+    awareness and customer engagement since partnering with
+    them.`,
+    name: "Emily Carter",
+    title: "Founder",
+    company: "Bloom Beauty",
+    image: "/team_icons/team2.svg"
+  },
+  {
+    quote: `  Our Marketing Agency has been instrumental in helping us
+    navigate the complexities of digital marketing. Their
+    expertise in SEO, PPC, and social media marketing has
+    significantly improved our online visibility and generated
+    quality leads for our business. What sets Our Marketing
+    apart is their commitment to delivering results and their
+    proactive approach to optimizing campaigns for maximum ROI.
+    I highly recommend their services to any business looking to
+    stay ahead in today's competitive landscape.`,
+    name: "David Thompson",
+    title: "COO",
+    company: "GreenTech Solutions",
+    image: "/team_icons/team3.svg"
+  },
+]
+
 const Testimonials = () => {
   return (
     <section className="bg-secondBg">
@@ -26,121 +88,17 @@ const Testimonials = () => {
           </div>
           <div className="p-6 xl:col-span-3">
             <div className="grid gap-4 md:grid-cols-2">
-              <div className="grid content-center gap-4">
-                <div className="p-6 rounded shadow-md shadow-myPurple-600 dark:bg-gray-900">
-                  <p>
-                    Working with Our Marketing Agency has been a game-changer
-                    for our company. Their innovative strategies and attention
-                    to detail have significantly boosted our online presence and
-                    customer engagement. The team at Our Marketing is not only
-                    highly skilled but also incredibly responsive and dedicated.
-                    I highly recommend their services to any business looking to
-                    take their marketing efforts to the next level
-                  </p>
-                  <div className="flex items-center mt-4 space-x-4">
-                    <Image
-                      src="/team_icons/team1.svg"
-                      alt="client1"
-                      width={50}
-                      height={50}
-                      layout="fit"
-                      className="w-12 h-12 rounded-full"
-                    />
-                    <div>
-                      <p className="text-lg font-semibold">Sarah Johnson</p>
-                      <p className="text-sm dark:text-gray-400">
-                        CEO, Techify Inc.
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <div className="p-6 rounded shadow-md shadow-myPurple-600 dark:bg-gray-900">
-                  <p>
-                    Our Marketing Agency exceeded our expectations in every
-                    aspect. From the initial consultation to the execution of
-                    our marketing campaigns, they demonstrated professionalism
-                    and expertise. Their ability to understand our brand and
-                    target audience resulted in tangible results, including
-                    increased website traffic and higher conversion rates. I'm
-                    extremely satisfied with the results and look forward to
-                    continuing our partnership
-                  </p>
-                  <div className="flex items-center mt-4 space-x-4">
-                    <Image
-                      src="/team_icons/team4.svg"
-                      width={50}
-                      height={50}
-                      alt="client3"
-                      layout="fit"
-                      className="w-12 h-12 rounded-full"
-                    />
-                    <div>
-                      <p className="text-lg font-semibold">Michael Smith</p>
-                      <p className="text-sm dark:text-gray-400">
-                        Marketing Manager, FreshBites
-                      </p>
-                    </div>
-                  </div>
-                </div>
+            <div className="grid content-center gap-4"> 
+      
+                {testimonialsData.slice(0, 2).map((testimonial, index) => (
+                  <TestimonialCard key={index} {...testimonial} />
+                ))}
               </div>
               <div className="grid content-center gap-4">
-                <div className="p-6 rounded shadow-md shadow-myPurple-600 dark:bg-gray-900">
-                  <p>
-                    As a small business owner, finding the right marketing
-                    partner was crucial for our growth. Our Marketing Agency
-                    came highly recommended, and they did not disappoint. Their
-                    team took the time to understand our unique needs and
-                    crafted customized strategies that aligned perfectly with
-                    our goals. Their creativity and attention to detail set them
-                    apart, and we've seen a significant improvement in brand
-                    awareness and customer engagement since partnering with
-                    them.
-                  </p>
-                  <div className="flex items-center mt-4 space-x-4">
-                    <Image
-                      src="/team_icons/team2.svg"
-                      alt=""
-                      width={50}
-                      height={50}
-                      layout="fit"
-                      className="w-12 h-12 rounded-full"
-                    />
-                    <div>
-                      <p className="text-lg font-semibold">Emily Carter</p>
-                      <p className="text-sm dark:text-gray-400">
-                        Founder, Bloom Beauty
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <div className="p-6 rounded shadow-md shadow-myPurple-600 dark:bg-gray-900">
-                  <p>
-                    Our Marketing Agency has been instrumental in helping us
-                    navigate the complexities of digital marketing. Their
-                    expertise in SEO, PPC, and social media marketing has
-                    significantly improved our online visibility and generated
-                    quality leads for our business. What sets Our Marketing
-                    apart is their commitment to delivering results and their
-                    proactive approach to optimizing campaigns for maximum ROI.
-                    I highly recommend their services to any business looking to
-                    stay ahead in today's competitive landscape.
-                  </p>
-                  <div className="flex items-center mt-4 space-x-4">
-                    <Image
-                      src="/team_icons/team3.svg"
-                      alt="client2"
-                      width={500}
-                      height={500}
-                      className="w-12 h-12 bg-center bg-cover rounded-full dark:bg-gray-500"
-                    />
-                    <div>
-                      <p className="text-lg font-semibold">David Thompson</p>
-                      <p className="text-sm dark:text-gray-400">
-                        COO, GreenTech Solutions
-                      </p>
-                    </div>
-                  </div>
-                </div>
+               
+                {testimonialsData.slice(2).map((testimonial, index) => (
+                  <TestimonialCard key={index} {...testimonial} />
+                ))}
               </div>
             </div>
           </div>
