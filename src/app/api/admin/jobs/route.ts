4124ec35e1e9f89eb9faeb3cbc2beede443f9154
Alex <alexgrubor@gmail.com>
2024-04-03 11:22:19 +0200

    Added Post method for creating job, test with postman passed

--- src/app/api/admin/jobs/route.ts
+++ src/app/api/admin/jobs/route.ts
@@ -19,3 +19,52 @@ export async function GET (request: NextRequest){
     
 }
 
+export async function POST(
+    req: Request
+  ) {
+    const requestBody = await req.json();
+  
+    const client = new Client({
+      connectionString: process.env.DATABASE_URL,
+    });
+  
+    try {
+      await client.connect();
+  
+      const query = `
+        INSERT INTO jobs (
+          title,
+          company_intro,
+          responsibilities,
+          requirements,
+          benefits,
+          contact_person,
+          contact_email,
+          status
+        ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+        RETURNING job_id  
+      `;
+      const values = [
+        requestBody.title,
+        requestBody.company_intro,
+        requestBody.responsibilities,
+        requestBody.requirements,
+        requestBody.benefits,
+        requestBody.contact_person,
+        requestBody.contact_email,
+        requestBody.status || 'open' 
+      ];
+  
+      const result = await client.query(query, values);
+      const newJobId = result.rows[0].job_id;
+  
+      return NextResponse.json({ jobId: newJobId }, { status: 201 });
+  
+    } catch (error) {
+      console.error('Error creating job:', error);
+      return NextResponse.json({ message: 'Error creating job' }, { status: 500 });
+    } finally {
+      await client.end();
+    }
+  }
+  
