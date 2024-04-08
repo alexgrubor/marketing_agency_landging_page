f531ab8c80be8abdb20c92ba7308f91768416330
Alex <alexgrubor@gmail.com>
2024-04-08 15:56:28 +0200

    submit contact form api route made and tested with postman

---
+++ src/app/api/submit-form/route.ts
@@ -0,0 +1,58 @@
+import { NextRequest, NextResponse } from "next/server";
+import { Client } from "pg";
+export async function POST(req: NextRequest) {
+  try {
+    const formData = await req.json();
+    const client = new Client({
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    });
+
+    await client.connect();
+
+    const result = await client.query(
+      `INSERT INTO contact_submissions (
+            name, 
+            first_name, 
+            last_name, 
+           email, 
+            company, 
+            website, 
+            company_stage, 
+            interested_in, 
+            budget, 
+            message, 
+            how_did_you_hear
+        ) VALUES (
+            $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11
+        ) RETURNING id;`,
+      [
+        formData.name,
+        formData.first_name,
+        formData.last_name,
+        formData.email,
+        formData.company,
+        formData.website,
+        formData.company_stage,
+        formData.interested_in,
+        formData.budget,
+        formData.message,
+        formData.how_did_you_hear,
+      ]
+    );
+    await client.end();
+
+    return NextResponse.json({
+      message: "Form submitted successfully",
+      id: result.rows[0].id,
+    });
+  } catch (error) {
+    console.error("Error submitting form:", error);
+    return NextResponse.json(
+      { error: "Form submission failed" },
+      { status: 500 }
+    );
+  }
+}
